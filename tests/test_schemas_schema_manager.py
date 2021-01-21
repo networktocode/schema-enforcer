e61b4933c7442c75fda333f615a32c2212d784ba
Chip Nielsen <chip.nielsen@networktocode.com>
2021-01-21 08:06:50 -0700

    Refactor tests to load from files

--- tests/test_schemas_schema_manager.py
+++ tests/test_schemas_schema_manager.py
@@ -5,10 +5,10 @@ import pytest
 from schema_enforcer.schemas.manager import SchemaManager
 from schema_enforcer.config import Settings
 
-FIXTURE_DIR = os.path.join(os.path.dirname(os.path.realpath(__file__)), "fixtures", "test_instances")
+FIXTURE_DIR = os.path.join(os.path.dirname(os.path.realpath(__file__)), "fixtures")
 
 CONFIG = {
-    "main_directory": os.path.join(FIXTURE_DIR, "schema"),
+    "main_directory": os.path.join(FIXTURE_DIR, "test_instances", "schema"),
     "data_file_search_directories": [os.path.join(FIXTURE_DIR, "hostvars")],
     "schema_mapping": {"dns.yml": ["schemas/dns_servers"]},
 }
@@ -27,160 +27,13 @@ def schema_manager():
     return schema_manager
 
 
-def test_dump(capsys, schema_manager):
-    """ Test validates schema dump for all schemas when no schema-id is provided. """
+@pytest.mark.parametrize("schema_id, result_file", [(None, "all.txt"), ("schemas/dns_servers", "byid.txt")])
+def test_dump(capsys, schema_manager, schema_id, result_file):
+    """ Test validates schema dump for multiple parameters. """
 
-    print_string = """{
-  "$schema": "http://json-schema.org/draft-07/schema#",
-  "$id": "schemas/dns_servers",
-  "description": "DNS Server Configuration schema.",
-  "type": "object",
-  "properties": {
-    "dns_servers": {
-      "type": "array",
-      "items": {
-        "type": "object",
-        "properties": {
-          "name": {
-            "type": "string"
-          },
-          "address": {
-            "type": "string",
-            "format": "ipv4"
-          },
-          "vrf": {
-            "type": "string"
-          }
-        },
-        "required": [
-          "address"
-        ]
-      },
-      "uniqueItems": true
-    }
-  },
-  "required": [
-    "dns_servers"
-  ]
-}
-{
-  "$schema": "http://json-schema.org/draft-07/schema#",
-  "$id": "schemas/ntp",
-  "description": "NTP Configuration schema.",
-  "type": "object",
-  "properties": {
-    "ntp_servers": {
-      "type": "array",
-      "items": {
-        "type": "object",
-        "properties": {
-          "name": {
-            "type": "string"
-          },
-          "address": {
-            "type": "string",
-            "format": "ipv4"
-          },
-          "vrf": {
-            "type": "string"
-          }
-        },
-        "required": [
-          "address"
-        ]
-      },
-      "uniqueItems": true
-    },
-    "ntp_authentication": {
-      "type": "boolean"
-    },
-    "ntp_logging": {
-      "type": "boolean"
-    }
-  },
-  "additionalProperties": false,
-  "required": [
-    "ntp_servers"
-  ],
-  "something": "extra"
-}
-{
-  "$schema": "http://json-schema.org/draft-07/schema#",
-  "$id": "schemas/syslog_servers",
-  "description": "Syslog Server Configuration schema.",
-  "type": "object",
-  "properties": {
-    "syslog_servers": {
-      "type": "array",
-      "items": {
-        "type": "object",
-        "properties": {
-          "name": {
-            "type": "string"
-          },
-          "address": {
-            "type": "string",
-            "format": "ipv4"
-          },
-          "vrf": {
-            "type": "string"
-          }
-        },
-        "required": [
-          "address"
-        ]
-      },
-      "uniqueItems": true
-    }
-  },
-  "required": [
-    "syslog_servers"
-  ]
-}
-"""
-    schema_manager.dump_schema(None)
-    captured = capsys.readouterr()
-    assert captured.out == print_string
-
-
-def test_dump_with_id(capsys, schema_manager):
-    """ Test validates schema dump for a single schema when schema-id is provided. """
-
-    print_string = """{
-  "$schema": "http://json-schema.org/draft-07/schema#",
-  "$id": "schemas/dns_servers",
-  "description": "DNS Server Configuration schema.",
-  "type": "object",
-  "properties": {
-    "dns_servers": {
-      "type": "array",
-      "items": {
-        "type": "object",
-        "properties": {
-          "name": {
-            "type": "string"
-          },
-          "address": {
-            "type": "string",
-            "format": "ipv4"
-          },
-          "vrf": {
-            "type": "string"
-          }
-        },
-        "required": [
-          "address"
-        ]
-      },
-      "uniqueItems": true
-    }
-  },
-  "required": [
-    "dns_servers"
-  ]
-}
-"""
-    schema_id = "schemas/dns_servers"
+    test_file = os.path.join(FIXTURE_DIR, "test_manager", "dump", result_file)
+    with open(test_file) as res_file:
+        expected = res_file.read()
     schema_manager.dump_schema(schema_id)
     captured = capsys.readouterr()
-    assert captured.out == print_string
+    assert captured.out == expected
