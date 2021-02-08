6e98b64821f93aebc041c5e15354df11615ee626
Chip Nielsen <chip.nielsen@networktocode.com>
2021-02-08 10:23:06 -0700

    Fix pylint and yamllint errors

--- tests/fixtures/test_validators/validators/check_interfaces.py
+++ tests/fixtures/test_validators/validators/check_interfaces.py
@@ -1,7 +1,10 @@
 # flake8: noqa
 # pylint: skip-file
 class CheckInterface(JmesPathModelValidation):
+    top_level_properties = ["interfaces"]
+    id = "CheckInterface"
     model = "interfaces"
     left = "interfaces.*[@.type=='core'][] | length([?@])"
     right = 2
     operator = "eq"
+    error = "Less than two core interfaces"
