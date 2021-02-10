3c961ee28d87e118810c6bcc286563863e4aeb12
Chip Nielsen <chip.nielsen@networktocode.com>
2021-02-10 09:38:12 -0700

    Disable pylint rule at file level

--- tests/test_schemas_validator.py
+++ tests/test_schemas_validator.py
@@ -1,6 +1,7 @@
 """
 Tests for validator plugin support
 """
+# pylint: disable=redefined-outer-name
 import os
 import pytest
 from schema_enforcer.ansible_inventory import AnsibleInventory
@@ -14,15 +15,15 @@ def inventory():
     """ Fixture for Ansible inventory used in tests """
     inventory_dir = os.path.join(FIXTURE_DIR, "inventory")
 
-    inventory = AnsibleInventory(inventory_dir)  # pylint: disable=redefined-outer-name
+    inventory = AnsibleInventory(inventory_dir)
     return inventory
 
 
 @pytest.fixture
-def host_vars(inventory):  # pylint: disable=redefined-outer-name
+def host_vars(inventory):
     """ Fixture for providing Ansible host_vars as a consolidated dict """
     hosts = inventory.get_hosts_containing()
-    host_vars = dict()  # pylint: disable=redefined-outer-name
+    host_vars = dict()
     for host in hosts:
         hostname = host.get_vars()["inventory_hostname"]
         host_vars[hostname] = inventory.get_host_vars(host)
@@ -38,19 +39,19 @@ def validators():
     return v.load_validators(validator_path)
 
 
-def test_jmespathvalidation_pass(host_vars, validators):  # pylint: disable=W0621
+def test_jmespathvalidation_pass(host_vars, validators):
     validate = getattr(validators["CheckInterface"], "validate")
     result = validate(host_vars["az_phx_pe01"], False)
     assert result[0].passed()
 
 
-def test_jmespathvalidation_fail(host_vars, validators):  # pylint: disable=W0621
+def test_jmespathvalidation_fail(host_vars, validators):
     validate = getattr(validators["CheckInterface"], "validate")
     result = validate(host_vars["az_phx_pe02"], False)
     assert not result[0].passed()
 
 
-def test_modelvalidation_pass(host_vars, validators):  # pylint: disable=W0621
+def test_modelvalidation_pass(host_vars, validators):
     validate = getattr(validators["CheckPeers"], "validate")
     result = validate(host_vars, False)
     assert result[0].passed()
