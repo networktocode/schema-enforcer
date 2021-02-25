ad3182710d2fd74da4fd2c0160d74db4cf9ed59c
Chip Nielsen <chip.nielsen@networktocode.com>
2021-02-25 15:54:06 -0700

    Update example validator

--- examples/ansible3/validators/check_interfaces.py
+++ examples/ansible3/validators/check_interfaces.py
@@ -3,12 +3,14 @@ from schema_enforcer.schemas.validator import JmesPathModelValidation
 
 
 class CheckInterface(JmesPathModelValidation):  # pylint: disable=too-few-public-methods
-    """Check that each device has at least two core uplinks."""
+    """Check that each device has more than one core uplink."""
 
-    top_level_properties = ["interfaces"]
-    id = "CheckInterface"
-    model = "interfaces"
-    left = "interfaces.*[@.type=='core'][] | length([?@])"
-    right = 2
-    operator = "gte"
-    error = "Less than two core interfaces"
+    def __init__(self):
+        """Initialize vars required for jmespath validation."""
+        super().__init__()
+        self.top_level_properties = ["interfaces"]
+        self.id = "CheckInterface"  # pylint: disable=invalid-name
+        self.left = "interfaces.*[@.type=='core'][] | length([?@])"
+        self.right = 2
+        self.operator = "gte"
+        self.error = "Less than two core interfaces"
