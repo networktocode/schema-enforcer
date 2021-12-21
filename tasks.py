b71a56783fca2efc3263a8affc6c604820ea23b1
Damien Garros <dgarros@gmail.com>
2021-12-21 16:31:15 -0500

    fix os getenv

--- tasks.py
+++ tasks.py
@@ -59,7 +59,7 @@ def _get_image_name(with_ansible=False):
     Returns:
         str: Name of container image. Includes tag.
     """
-    if with_ansible and not os.getenv("GITHUB_ACTION", False):
+    if with_ansible and not os.getenv("GITHUB_ACTION", None):
         name = f"{IMAGE_NAME}:{IMAGE_VER}-{ANSIBLE_PACKAGE}{ANSIBLE_VER}"
     else:
         name = f"{IMAGE_NAME}:{IMAGE_VER}"
